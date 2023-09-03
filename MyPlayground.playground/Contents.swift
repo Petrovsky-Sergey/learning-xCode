6f5057523234351d80a8f5ad29031bddbd73c310
PetrOFFsky <petrovsky.dev@gmail.com>
2023-09-03 17:24:58 +0300

    Initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,147 @@
+//var greetingOne = "Hello Playground"
+//
+//let greetingTwo = "Hello Steve"
+//
+//
+//greetingTwo
+//
+//greetingOne = "Hello World"
+//
+//
+//greetingOne
+//greetingTwo
+//
+//
+//// Part 3
+//
+//var greeting: String = "Hello Serega"
+//greeting = "10"
+//
+//// Character
+//
+//let quizAnswer: Character = "a"
+//
+//var name = ""
+//
+//// Integer
+//var age = 0
+//age = 10
+//
+//// Double
+//var number = 10.1
+//
+//// Float
+//var somefloat: Float = 0.0
+//somefloat = 1.2345678
+//
+//// Bool
+//let booleanValue = false
+//
+//let sum = Double(age) + number
+//
+//String(age)
+//let someString = "12"
+//let sumeOfIntegers = age + (Int(someString) ?? 0)
+//
+//
+//// Part 4
+//
+////var number = 5
+//number = 6
+//
+//let value = true
+//
+//var a = 4
+//var b = 3
+//a * b
+//a - b
+//a + b
+//a / b
+//
+//let c = a + b
+//
+//
+//a = a + 1
+//a += 1
+//
+//a -= 1
+//
+//
+//let d = 13 % 5
+//
+//
+//a = 14
+//b = 8
+//
+//a > b
+//a < b
+//a >= b
+//a <= b
+//a == b
+//a != b
+//
+//
+//var time = 8
+//var temp = 20
+//
+//time = 15
+//
+//time <= 12 && temp >= 18
+//
+//
+//time = 8
+//temp = 10
+//
+//
+//let answer = time <= 12 || temp >= 18
+//!answer
+
+
+// Part 5
+
+//let name = "Tim"
+//let surname = "Cook"
+//
+//var fullName = name + " " + surname
+//var hello = "Hello, my name is "
+//hello += fullName
+//
+//
+//let info = "Tim Cook is CEO of Apple"
+//
+//print(info)
+//
+//fullName = "Serega"
+//let newInfo = "\(fullName) is CEO of Apple"
+//print(newInfo)
+//
+//
+//let a = 5
+//let b = 3
+//let infoAbouSum = "the sum of a and b is \(a + b)"
+//
+//
+//let multiString =
+//"""
+//some text
+//some text
+//some text
+//some text
+//some text
+//"""
+//print(multiString)
+
+
+// Part 6
+
+var numder = 4
+if numder > 4 {
+    print("true")
+} else if numder < 4 {
+    print("NO")
+} else {
+    print("END")
+}
+
+
+
